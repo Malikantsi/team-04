cc4d3b718dc6ead3b2c8eb42731989cf5dbab8c2
alandmr <fenix_3006@hotmail.com>
2025-09-18 18:17:32 -0400

    Update product.js - Commented out code removed
    Commented out code removed

--- src/js/product.js
+++ src/js/product.js
@@ -9,27 +9,3 @@ const product = new ProductDetails(productId, dataSource);
 product.init();
 
 console.log(dataSource.findProductById(productId));
-
-// function addProductToCart(product) {
-//   // Retrieve the existing cart data or initialize an empty array if none exists
-//   let cartItems = getLocalStorage("so-cart");
-
-//   if (!Array.isArray(cartItems)) {
-//     // Ensure cart is an array
-//     cartItems = [];
-//   }
-//   // Add the new product to the cart array
-//   cartItems.push(product);
-//   // Store the updated array as a JSON string
-//   setLocalStorage("so-cart", cartItems);
-// }
-// add to cart button event handler
-// async function addToCartHandler(e) {
-//   const product = await dataSource.findProductById(e.target.dataset.id);
-//   addProductToCart(product);
-// }
-
-// // add listener to Add to Cart button
-// document
-//   .getElementById("addToCart")
-//   .addEventListener("click", addToCartHandler);
