d170d57287c10f5bd19001e66d32654521890abd
kotozo <toz20002@byui.edu>
2025-09-26 15:13:55 +0000

    Changes Update 26092025

--- src/js/ProductList.mjs
+++ src/js/ProductList.mjs
@@ -1,3 +1,17 @@
+import { renderListwithTemplate } from './utils.mjs';
+
+function productCardTemplate(product) {
+  return
+  `<li class="product-card">
+    <a href ="product_pages/?products=${product.id}">
+      <img src="${product.image}" alt="${product.name}">
+      <h2>${product.Brand.Nameame}</h2>
+      <h3>${product.Name}</h3>
+      <p class="product-card__price">${product.FinalPrice}</p>
+    </a>
+  </li>`;
+
+}
 export default class ProductList {
   constructor(category, dataSource, listElement) {
     this.category = category;
@@ -6,5 +20,14 @@ export default class ProductList {
   }
     async init() {
         const products = await this.dataSource.getData();
+        this.renderList(list);
+    }
+
+    renderList(list) {
+      //const htmlStrings = list.map(productCardTemplate);
+      //this.listElement.isertAdjacentHTML('afterbegin', htmlStrings.join(''));
+
+      //apply use new utulity function instead of the commented code above
+      renderListwithTemplate(productCardTemplate, list, this.listElement);
     }
 }
--- src/js/main.js
+++ src/js/main.js
@@ -4,3 +4,7 @@ import ProductList from "./ProductList.mjs";
 // create an instance of ProductData for tents
 const dataSource = new ProductData("tents");
 //create an instance of productList
+const productList = new ProductList(dataSource, "productList");
+//load the product data and display it
+productList.loadData();
+productList.display();
